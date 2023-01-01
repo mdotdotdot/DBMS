7cd6926d6cd73dd23bc79e2982c49773f2425a60
mdotdotdot <116056922+mdotdotdot@users.noreply.github.com>
2023-01-02 00:35:53 +0500

    Update dbmsproj.py

--- dbmsproj.py
+++ dbmsproj.py
@@ -2,6 +2,7 @@ from tkinter import *
 import mysql.connector
 from tkinter import messagebox
 
+
 def log_out(frame):
     frame.pack_forget()
     mainframe.pack()
@@ -338,6 +339,137 @@ def course_details_view(frame,username,acc_type):
         command=lambda: log_out(coursesframe)
     )
     logoutbutton2.grid(row=8,column=8)
+# ///////////////////////////////////Maliha Addition/////////////////////////////////////////////////////
+def add_instructor_view(frame):
+    frame.pack_forget()
+    addiframe = Frame(my_window)
+    addiframe.pack()
+    prompt1 = Label(addiframe,text='Fill the personal details:')
+    prompt1.pack(padx=0,pady=10)
+    username_label = Label(addiframe,text="Username:")
+    username_entry = Entry(addiframe)
+    password_label = Label(addiframe,text="Password:")
+    password_entry = Entry(addiframe)
+    name_label = Label(addiframe,text="Name:")
+    name_entry = Entry(addiframe)
+    id_label = Label(addiframe,text="ID:")
+    id_entry = Entry(addiframe)
+    email_label = Label(addiframe,text="Email:")
+    email_entry = Entry(addiframe)
+    address_label = Label(addiframe,text="Address:")
+    address_entry = Entry(addiframe)
+    phone_label = Label(addiframe,text="Contact Number:")
+    phone_entry = Entry(addiframe)
+    joining_label = Label(addiframe,text="Joining Date(YYYY/MM/DD):")
+    joining_entry = Entry(addiframe)
+    username_label.pack()
+    username_entry.pack()
+    password_label.pack()
+    password_entry.pack()
+    name_label.pack()
+    name_entry.pack()
+    id_label.pack()
+    id_entry.pack()
+    email_label.pack()
+    email_entry.pack()
+    address_label.pack()
+    address_entry.pack()
+    phone_label.pack()
+    phone_entry.pack()
+    joining_label.pack()
+    joining_entry.pack()
+    submit_info = Button(
+        addiframe,
+        text = "Submit",
+        command=lambda :verify_add_instructors1(username_entry.get(),id_entry.get(),phone_entry.get(),addiframe)
+    )
+    submit_info.pack()
+    backbutton = Button(
+        addiframe,
+        text='Back',
+        command=lambda:close_courses_back(frame,"admin","adminstrator")
+    )
+    backbutton.pack()
+
+def verify_add_instructors1(username,id,phone,frame):
+    flag = 0
+    if len(phone) != 11:
+        messagebox.showerror('Error', 'Invalid contact number.')
+        flag = 1
+    db = connect()
+    cursor = db.cursor()
+    query = "SELECT username FROM login WHERE username = '"+username+"'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    if result is not None:
+        messagebox.showerror('Error', 'Invalid username already assigned.')
+        flag = 1
+    cursor.close()
+    db.close()
+    db = connect()
+    cursor = db.cursor()
+    query = "SELECT Instructor_id FROM instructor where Instructor_id = '"+id+"'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    if result is not None:
+        messagebox.showerror('Error', 'ID already assigned.')
+        flag = 1
+    cursor.close()
+    db.close()
+    if flag==0:
+        add_more_info(frame)
+
+def changeflagandproceed(frame,flag):
+    frame.pack_forget()
+    flag=0
+
+def add_more_info(frame):
+    # loop=1
+    # while loop:
+    frame.pack_forget()
+    malihqudsiframe = Frame(my_window)
+    malihqudsiframe.pack()
+    prompt1 = Label(malihqudsiframe,text='Fill Instructor details')
+    prompt1.pack(padx=0,pady=10)
+    prompt2 = Label(malihqudsiframe,text='Job Description:')
+    prompt2.pack(padx=0,pady=10)
+    options = ["Course Instructor","Researcher","Both"]
+    clicked = StringVar()
+    clicked.set( "Course Instructor" )
+    drop1 = OptionMenu(malihqudsiframe , clicked , *options)
+    drop1.pack(padx=10,pady=10)
+    extraf1 = Frame(malihqudsiframe)
+    extraf2 = Frame(malihqudsiframe)
+    prompt10= Label(extraf1,text="Years of Experience: ")
+    prompt10.pack(padx=10,pady=10)
+    prompt10entry = Entry(extraf1)
+    prompt10entry.pack(padx=10,pady=10)
+    prompt11= Label(extraf1,text="Course : ")
+    prompt11.pack(padx=10,pady=10)
+    courseoptions = ["Database Systems","Calculus","Linear Algebra", "Mechanics"]
+    clickedcourse = StringVar()
+    clickedcourse.set( "Database Systems" )
+    drop2 = OptionMenu(extraf1 , clickedcourse , *courseoptions)
+    drop2.pack(padx=10,pady=10)
+    prompt20= Label(extraf2,text="Research Field:")
+    prompt20.pack(padx=10,pady=10)
+    prompt20entry = Entry(extraf2)
+    prompt20entry.pack(padx=10,pady=10)
+    prompt21= Label(extraf2,text="Field Description:")
+    prompt21.pack(padx=10,pady=10)
+    if clicked=="Course Instructor":
+        extraf1.pack()
+    elif clicked=="Researcher":
+        extraf2.pack()
+    else:
+        extraf1.pack()
+        extraf2.pack()
+    submitbutton = Button(malihqudsiframe,text="Submit", command=lambda:changeflagandproceed(frame,flag))
+    submitbutton.pack()
+
+
+
+
 
 def logged_in(usertype,mainframe,username):
     mainframe.pack_forget()
@@ -347,6 +479,7 @@ def logged_in(usertype,mainframe,username):
         instructor_menu(username)
     elif usertype=='administrator':
         admin_menu()
+
 
 
 def backtomainscreen(frame):
@@ -389,6 +522,12 @@ def admin_menu():
         command=lambda: session_details_view(adminframe)
     )
     look_at_sessions.grid(row=25,column=2)
+    add_instructor=Button(
+        adminframe,
+        text="Add Instructor",
+        command= lambda: add_instructor_view(adminframe)
+    )
+    add_instructor.grid(row=30,column=2)
 
 def instructor_menu(username):
     frameinstructor=Frame(my_window)
@@ -474,10 +613,10 @@ def trainee_menu(username):
     prompt_1.grid(row=0,column=0)
     db = connect()
     cursor = db.cursor()
-    query = "SELECT t_id,first_name,last_name,contact_no,email,joining_date,address,time_slot,course_name FROM (((trainee JOIN login using (username)) JOIN session using(session_id)) JOIN course using(course_id)) WHERE username ='"+username+"'"
-    # values = (username)
+    # query = "SELECT t_id,first_name,last_name,contact_no,email,joining_date,address,time_slot,course_name FROM (((trainee JOIN login using (username)) JOIN session using(session_id)) JOIN course using(course_id)) WHERE username ='"+username+"'"
+    query = "SELECT * from student where username='"+username+"'"
     cursor.execute(query)
-    result = cursor.fetchall()
+    result = cursor.fetchone()
 
     tidlabel1 = Label(frametrainee, text='Trainee ID:')
     tidlabel1.grid(row=1, column=0)
@@ -495,21 +634,21 @@ def trainee_menu(username):
     time_slot1.grid(row=1, column=6)
     coursename1 = Label(frametrainee, text='Course Name:')
     coursename1.grid(row=1, column=7)
-    tidlabel2 = Label(frametrainee,text=result[0][0])
+    tidlabel2 = Label(frametrainee,text=result[0])
     tidlabel2.grid(row=2, column=0)
-    fname2 = Label(frametrainee,text=result[0][1]+result[0][2])
+    fname2 = Label(frametrainee,text=result[1]+" "+result[2])
     fname2.grid(row=2, column=1)
-    contact2 = Label(frametrainee,text=result[0][3])
+    contact2 = Label(frametrainee,text=result[3])
     contact2.grid(row=2, column=2)
-    email2 = Label(frametrainee,text=result[0][4])
+    email2 = Label(frametrainee,text=result[4])
     email2.grid(row=2, column=3)
-    joiningdate2 = Label(frametrainee,text=result[0][5])
+    joiningdate2 = Label(frametrainee,text=result[5])
     joiningdate2.grid(row=2, column=4)
-    address2 = Label(frametrainee,text=result[0][6])
+    address2 = Label(frametrainee,text=result[6])
     address2.grid(row=2, column=5)
-    time_slot2 = Label(frametrainee,text=result[0][7])
+    time_slot2 = Label(frametrainee,text=result[7])
     time_slot2.grid(row=2, column=6)
-    coursename2 = Label(frametrainee,text=result[0][8])
+    coursename2 = Label(frametrainee,text=result[8])
     coursename2.grid(row=2, column=7)
     cursor.close()
     db.close()
